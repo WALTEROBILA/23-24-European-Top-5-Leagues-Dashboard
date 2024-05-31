a81a317dde0f3c6b4f8d1eff70d070b7ee84bc28
WALTEROBILA <aderobila@gmail.com>
2024-05-31 11:10:42 +0300

    Functions with Docstring

--- functions.py
+++ functions.py
@@ -7,12 +7,30 @@ from soccerplots.radar_chart import Radar
 
 
 def clean_standard(std):
+    """
+    Cleans "Standard Stats" data, renaming columns.\n
+
+    Args:
+        Standard Stats data. \n
+    
+    Returns:
+        Renamed Standard Stats data.
+    """
     std.rename(columns={"Gls":"Goals Scored", "Ast":"Assists","G+A":"Goals + Assists","Min":"Minutes Played", "xG":"Expected Goals","xAG":"Expected Assists","PK":"Penalties Scored","PKatt":"Penalties Attempted","CrdY":"Yellow Cards", "CrdR":"Red Cards",
                          "PrgC":"Progressive Carries","PrgP":"Progressive Passes","PrgR":"Progressive Passes Received", "G-PK":"Non-Penalty Goals",
                           "npxG":"Non-Penalty Expected Goals", }, inplace=True)
     return std
 
 def clean_poss(poss):
+    """
+    Cleans "Possession" data, renaming columns and dropping a few from the data.\n
+
+    Args:
+        Possession data. \n
+    
+    Returns:
+        Cleaned Possesion data.
+    """
     poss.rename(columns={"Att 3rd":"Touches in the Attacking Third", "TotDist":"Total Carry Distance","Att":"Dribbles Attempted", "Succ%":"Dribble Success %",
                          "1/3":"Carries Into the Final Third", "CPA":"Carries into the Penalty Area","Rec":"Passes Received",
                          "PrgR":"Progressive Passes Received","Def Pen":"Touches in the Defensive Penalty Area",
@@ -21,6 +39,15 @@ def clean_poss(poss):
     return poss
 
 def clean_pass(pas):
+    """
+    Cleans "Passing" data, renaming columns and dropping a few.\n
+
+    Args:
+        Passing data. \n
+    
+    Returns:
+        Cleaned Passing data.
+    """
     pas.rename(columns={"KP":"Key Passes","CrsPA":"Crosses into the Penalty Area","PPA":"Passes into the Penalty Area",
                         "A-xAG":"Assists Overperformance","Att":"Passes Attempted",
                         "Cmp%":"Pass Completion %"},inplace=True)
@@ -28,16 +55,43 @@ def clean_pass(pas):
     return pas
 
 def clean_ptypes(ptype):
+    """
+    Cleans "Pass Types" data, renaming columns and dropping a few.\n
+
+    Args:
+        Pass Types data. \n
+    
+    Returns:
+        Cleaned Pass Types data.
+    """
     ptype.rename(columns={"Sw":"Switches"},inplace=True)
     ptype.drop(columns=["Pos","Age","Born","90s","Nation","Att","Matches"], inplace=True)
     return ptype
 
 def clean_misc(misc):
+    """
+    Cleans miscellanaous data, renaming columns and dropping a few.\n
+
+    Args:
+        Miscellanous data. \n
+    
+    Returns:
+        Cleaned Miscellanous data.
+    """
     misc.rename(columns={"Recov":"Recoveries","Won%":"Aerial Duel Success Rate","Fls":"Fouls", "'Int":"Interceptions", "Fld":"Fouls Drawn","PKwon":"Penalty Kicks Won"}, inplace=True)
     misc.drop(columns=["Pos","Age","Born","90s","Nation","Matches"], inplace=True)
     return misc
     
 def clean_def_actions(defe):
+    """
+    Cleans "defensive actions" data, renaming columns, dropping a few and generating a new feature.\n
+
+    Args:
+        Defensive Actions data. \n
+    
+    Returns:
+        Cleaned Defensive Actions data.
+    """
     defe.rename(columns={"Tkl":"Tackles"},inplace=True)
     defe.drop(columns=["Pos","Age","Born","90s","Nation","Matches"], inplace=True)
     defe['Tackle Success Rate'] = (defe['TklW']/defe["Tackles"]) * 100
@@ -45,6 +99,18 @@ def clean_def_actions(defe):
 
 
 def merge_stats(league, std, poss, pas, ptype, misc, defe):
+    """
+    Combines the standard stats, possession, passing, pass types, miscellanous and defensive actions data, merging them into 
+    a single dataframe. Also adds a "League" variable to the data, specifying the league in which the player plays, as at
+    the time of collection of the data.\n
+
+    Args:
+        league=league
+        std, poss, pas, ptype, misc, and defe dataframes. All should have been passed through the functions.\n
+
+    Returns:
+        A dataframe with all all attributes stated above, as well as the league. 
+    """
     std = clean_standard(std)
     poss = clean_poss(poss)
     pas = clean_pass(pas)
@@ -62,11 +128,29 @@ def merge_stats(league, std, poss, pas, ptype, misc, defe):
 
 
 def merge_leagues(pl,li):
+    """
+    Merges dataframes from different leagues.\n
+
+    Args:
+        pl,li,ll,sa,bl = Premier League, Liggue 1, La Liga, Serie A, Bundesliga dataframes.\n
+
+    Returns:
+        A unified dataframe with all leagues.
+    """
     data = pd.concat([pl,li], axis=0)
     print(data["League"].unique())
     return data
 
 def scatter_variables(data):    # Variables to appear on the x,y scatterplot
+    """
+    A function  that defines the data that will appear on the scatterplots, as well as the radar plots. The data should ideally be per 90. \n
+
+    Args:
+        A cleaned dataset that contains data from various leagues.\n
+
+    Returns:
+        Data to be plotted on the scatterplots as well as comparative radar plots.
+    """
     dat = data[["Player","Squad","Goals Scored","Assists","Goals + Assists", "Non-Penalty Goals","Penalties Scored","Penalties Attempted",
                "Yellow Cards", "Red Cards","Expected Goals","Non-Penalty Expected Goals","Expected Assists",
                "Progressive Carries","Progressive Passes","Progressive Passes Received_x","Touches","Touches in the Defensive Penalty Area",
